5dd715d7dfbcf449aa2ce60b378f84a1eeccce38
Ralph Irving <ralph_irving@hotmail.com>
2015-01-01 16:13:38 +0000

    Update copyright year. Increase squeezelite 1.3 revision to 523.

--- flac.c
+++ flac.c
@@ -1,7 +1,7 @@
 /* 
  *  Squeezelite - lightweight headless squeezeplay emulator for linux
  *
- *  (c) Adrian Smith 2012, triode1@btinternet.com
+ *  (c) Adrian Smith 2012-2015, triode1@btinternet.com
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- squeezelite.h
+++ squeezelite.h
@@ -1,7 +1,7 @@
 /* 
  *  Squeezelite - lightweight headless squeezebox emulator
  *
- *  (c) Adrian Smith 2012, 2013, triode1@btinternet.com
+ *  (c) Adrian Smith 2012-2015, triode1@btinternet.com
  *  
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -20,7 +20,7 @@
 
 // make may define: PORTAUDIO, SELFPIPE or RESAMPLE to influence build
 
-#define VERSION "v1.3.11-520"
+#define VERSION "v1.3.11-523"
 
 // build detection
 #if defined(linux)
